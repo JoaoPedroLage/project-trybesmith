c956809299878cc7227daf373c2dd18e2814aa0e
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-04-01 10:27:33 -0300

    feat: create a index to export all routes

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import productsRouter from './productsRoute';
+import usersRouter from './usersRoute';
+import ordersRouter from './ordersRoute';
+// import loginRouter from './loginRoute';
+
+export {
+  productsRouter,
+  usersRouter,
+  ordersRouter,
+  // loginRouter,
+};
