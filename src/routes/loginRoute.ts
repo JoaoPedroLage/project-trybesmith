039fd9a0e6f570577f21d851868357aa75699418
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-04-01 10:28:06 -0300

    feat: create the login router (need a refactor)

---
+++ src/routes/loginRoute.ts
@@ -0,0 +1,16 @@
+// import { Request, Response, Router } from 'express';
+
+// import loginControllers from '../controllers/loginController';
+// import validationMiddleware from '../middlewares/validation.middleware';
+
+// const loginRouter = Router();
+
+// loginRouter.post(
+//   '/',
+//   validationMiddleware.validateLogin,
+//   async (req: Request, res: Response) => {
+//     await loginControllers.create(req, res);
+//   },
+// );
+
+// export default loginRouter;
