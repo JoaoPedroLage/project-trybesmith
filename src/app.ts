eac69a517c7e9bce4b3c48df6ebc3fac7dbb4c02
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-04-01 10:12:13 -0300

    feat: add address routes to use in the app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express from 'express';
 
+import 'dotenv/config';
+
+import { productsRouter, usersRouter, ordersRouter } from './routes';
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productsRouter);
+
+app.use('/users', usersRouter);
+
+app.use('/orders', ordersRouter);
+
+// app.use('/login', loginRouter);
 
 export default app;
